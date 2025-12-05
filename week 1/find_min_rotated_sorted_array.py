60a02335942da22871ad3519651fb2d2bf3d1514
aarsh1a <139792275+aarsh1a@users.noreply.github.com>
2025-12-05 23:33:27 +0530

    last q

---
+++ week 1/find_min_rotated_sorted_array.py
@@ -0,0 +1,12 @@
+#ccompare mid with r  if greater move l else r=mid when l==r it is minimum
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l=0
+        r= len(nums)-1
+        while l<r:
+            m =(l+r)//2
+            if nums[m]>nums[r]:
+                l=m+1
+            else:
+                r= m
+        return nums[l]
