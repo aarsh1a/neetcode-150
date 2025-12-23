cf1068a70836ba19ae449ee706cfb4e2381853ab
aarsh1a <139792275+aarsh1a@users.noreply.github.com>
2025-12-23 22:41:44 +0530

    day 23 i think

---
+++ week 1/last_stone_weight.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        heap=[]
+        for s in stones:
+            heapq.heappush(heap,-s)
+        while len(heap)>1:
+            a= -heapq.heappop(heap)
+            b= -heapq.heappop(heap)
+            if a!=b:
+                heapq.heappush(heap,-(a-b))
+        if heap:
+            return -heap[0]
+        return 0
